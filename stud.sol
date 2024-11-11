e7fecb3d50689f0a00624e66b9d4e99b3a9d2540
PIYUSH SHAH <piyush.class@gmail.com>
2024-11-11 15:20:09 +0530

    student

--- stud.sol
+++ stud.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// Write a program in solidity to create Student data. Use the following constructs: 1.Structures 2.Arrays 3.Fallback. Deploy this as smart contract on Ethereum and Observe the transaction fee and Gas values
 pragma solidity ^0.8.0;
 
 contract StudentData {
